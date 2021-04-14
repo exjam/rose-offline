dc47d1abdf2ea3a75357c57e221fd53f04f82749
James Benton <james.benton2@gmail.com>
2021-04-14 12:26:25 +0100

    Add Position::distance function to calculate distance between two positions.

--- src/game/components/position.rs
+++ src/game/components/position.rs
@@ -1,4 +1,5 @@
 use serde::{Deserialize, Serialize};
+use std::f32;
 
 #[derive(Clone, Deserialize, Serialize)]
 pub struct Position {
@@ -7,4 +8,13 @@ pub struct Position {
     pub z: u16,
     pub zone: u16,
     pub respawn_zone: u16,
+}
+
+impl Position {
+    pub fn distance(&self, other: &Position) -> f32 {
+        let dx = self.x - other.x;
+        let dy = self.y - other.y;
+        let dz = self.z as f32 - other.z as f32;
+        (dx * dx + dy * dy + dz * dz).sqrt()
+    }
 }
