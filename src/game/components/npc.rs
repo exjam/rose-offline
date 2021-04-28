adeeafaf472d00e90c76b7a3f2b19c148747e5f7
James Benton <james.benton2@gmail.com>
2021-04-28 09:11:32 +0100

    Add direction to Npc.

--- src/game/components/npc.rs
+++ src/game/components/npc.rs
@@ -1,13 +1,20 @@
 use crate::game::data::formats::ifo;
 
+#[derive(Clone)]
 pub struct Npc {
     pub id: u32,
+    pub quest_index: u16,
+    pub direction: f32,
 }
 
 impl From<&ifo::Npc> for Npc {
     fn from(npc: &ifo::Npc) -> Self {
+        let direction = npc.object.rotation.euler_angles().2.to_degrees();
+        // TODO: Get index from LIST_EVENT for npc.quest_file_name
         Self {
             id: npc.object.object_id,
+            direction: direction,
+            quest_index: 0,
         }
     }
 }
