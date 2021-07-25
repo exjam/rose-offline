f5140480cc438abf7a1b49b6eb476161453163c1
James Benton <james.benton2@gmail.com>
2021-07-25 11:19:32 +0100

    irose: Move PacketWriteItems / PacketReadItems trait to common_packets

--- src/irose/protocol/game/common_packets.rs
+++ src/irose/protocol/game/common_packets.rs
@@ -1,8 +1,13 @@
 use modular_bitfield::prelude::*;
+use num_traits::FromPrimitive;
 use std::convert::TryInto;
 
 use crate::{
-    game::components::HotbarSlot,
+    data::{
+        item::{EquipmentItem, Item, StackableItem},
+        ItemReference,
+    },
+    game::components::{Equipment, EquipmentIndex, HotbarSlot, Money},
     protocol::{PacketReader, PacketWriter, ProtocolError},
 };
 
@@ -40,4 +45,226 @@ pub fn write_hotbar_slot(writer: &mut PacketWriter, slot: &Option<HotbarSlot>) {
         .with_slot_type(slot_type)
         .with_index(index);
     writer.write_bytes(&slot.into_bytes());
+}
+
+#[bitfield]
+#[derive(Clone, Copy)]
+pub struct PacketEquipmentAmmoPart {
+    #[skip(getters)]
+    item_type: B5,
+    #[skip(getters)]
+    item_number: B10,
+    #[skip]
+    __: B1,
+}
+
+#[bitfield]
+#[derive(Clone, Copy)]
+pub struct PacketEquipmentItemPart {
+    #[skip(getters)]
+    item_number: B10,
+    #[skip(getters)]
+    gem: B9,
+    #[skip(getters)]
+    has_socket: bool,
+    #[skip(getters)]
+    grade: B4,
+}
+
+#[bitfield]
+#[derive(Clone, Copy)]
+pub struct PacketFullItemHeader {
+    #[skip(setters)]
+    item_type: B5,
+    #[skip(setters)]
+    item_number: B10,
+    #[skip]
+    __: B1,
+}
+
+#[bitfield]
+#[derive(Clone, Copy)]
+pub struct PacketEquipmentItemFull {
+    #[skip(getters)]
+    item_type: B5,
+    #[skip(getters)]
+    item_number: B10,
+    is_crafted: bool,
+    gem: B9,
+    durability: B7,
+    life: B10,
+    has_socket: bool,
+    is_appraised: bool,
+    grade: B4,
+}
+
+#[bitfield]
+#[allow(dead_code)]
+#[derive(Clone, Copy)]
+pub struct PacketStackableItemFull {
+    #[skip(getters)]
+    item_type: B5,
+    #[skip(getters)]
+    item_number: B10,
+    #[skip]
+    __: B1,
+    quantity: B32,
+}
+
+pub trait PacketReadItems {
+    fn read_item_full(&mut self) -> Result<Option<Item>, ProtocolError>;
+}
+
+impl<'a> PacketReadItems for PacketReader<'a> {
+    fn read_item_full(&mut self) -> Result<Option<Item>, ProtocolError> {
+        let item_bytes = self.read_fixed_length_bytes(6)?;
+        let item_header = PacketFullItemHeader::from_bytes(item_bytes[0..2].try_into().unwrap());
+        let item_number = item_header.item_number();
+        if item_number == 0 || item_number > 999 {
+            return Ok(None);
+        }
+
+        if let Some(item_type) = FromPrimitive::from_u8(item_header.item_type()) {
+            let item_reference = ItemReference::new(item_type, item_header.item_number() as usize);
+
+            if item_type.is_stackable() {
+                let stackable_item =
+                    PacketStackableItemFull::from_bytes(item_bytes.try_into().unwrap());
+                if let Some(item) = StackableItem::new(&item_reference, stackable_item.quantity()) {
+                    return Ok(Some(Item::Stackable(item)));
+                }
+            } else {
+                let equipment_item =
+                    PacketEquipmentItemFull::from_bytes(item_bytes.try_into().unwrap());
+                if let Some(mut item) = EquipmentItem::new(&item_reference) {
+                    item.gem = equipment_item.gem();
+                    item.durability = equipment_item.durability();
+                    item.life = equipment_item.life();
+                    item.grade = equipment_item.grade();
+                    item.is_crafted = equipment_item.is_crafted();
+                    item.has_socket = equipment_item.has_socket();
+                    item.is_appraised = equipment_item.is_appraised();
+                    return Ok(Some(Item::Equipment(item)));
+                }
+            }
+        }
+
+        Ok(None)
+    }
+}
+
+pub trait PacketWriteItems {
+    fn write_equipment_ammo_part(&mut self, item: Option<&StackableItem>);
+    fn write_equipment_item_part(&mut self, item: Option<&EquipmentItem>);
+    fn write_equipment_item_full(&mut self, equipment: Option<&EquipmentItem>);
+    fn write_equipment_visible_part(&mut self, equipment: &Equipment);
+    fn write_stackable_item_full(&mut self, stackable: Option<&StackableItem>);
+    fn write_item_full(&mut self, item: Option<&Item>);
+    fn write_item_full_money(&mut self, money: Money);
+}
+
+impl PacketWriteItems for PacketWriter {
+    fn write_equipment_ammo_part(&mut self, item: Option<&StackableItem>) {
+        if let Some(item) = item {
+            let part = PacketEquipmentAmmoPart::new()
+                .with_item_number(item.item.item_number as u16)
+                .with_item_type(item.item.item_type as u8);
+            for b in part.into_bytes().iter() {
+                self.write_u8(*b);
+            }
+        } else {
+            self.write_u16(0);
+        }
+    }
+
+    fn write_equipment_item_part(&mut self, item: Option<&EquipmentItem>) {
+        if let Some(item) = item {
+            let part = PacketEquipmentItemPart::new()
+                .with_item_number(item.item.item_number as u16)
+                .with_gem(item.gem)
+                .with_has_socket(item.has_socket)
+                .with_grade(item.grade);
+            for b in part.into_bytes().iter() {
+                self.write_u8(*b);
+            }
+            self.write_u8(0);
+        } else {
+            self.write_u32(0);
+        }
+    }
+
+    fn write_equipment_visible_part(&mut self, equipment: &Equipment) {
+        for index in &[
+            EquipmentIndex::Head,
+            EquipmentIndex::Body,
+            EquipmentIndex::Hands,
+            EquipmentIndex::Feet,
+            EquipmentIndex::Face,
+            EquipmentIndex::Back,
+            EquipmentIndex::WeaponRight,
+            EquipmentIndex::WeaponLeft,
+        ] {
+            self.write_equipment_item_part(equipment.get_equipment_item(*index));
+        }
+    }
+
+    fn write_equipment_item_full(&mut self, equipment: Option<&EquipmentItem>) {
+        match equipment {
+            Some(equipment) => {
+                let item = PacketEquipmentItemFull::new()
+                    .with_item_type(equipment.item.item_type as u8)
+                    .with_item_number(equipment.item.item_number as u16)
+                    .with_is_crafted(equipment.is_crafted)
+                    .with_gem(equipment.gem)
+                    .with_durability(equipment.durability)
+                    .with_life(equipment.life)
+                    .with_has_socket(equipment.has_socket)
+                    .with_is_appraised(equipment.is_appraised)
+                    .with_grade(equipment.grade);
+                self.write_bytes(&item.into_bytes());
+            }
+            _ => {
+                self.write_u16(0);
+                self.write_u32(0);
+            }
+        }
+    }
+
+    fn write_stackable_item_full(&mut self, stackable: Option<&StackableItem>) {
+        match stackable {
+            Some(stackable) => {
+                let item = PacketStackableItemFull::new()
+                    .with_item_type(stackable.item.item_type as u8)
+                    .with_item_number(stackable.item.item_number as u16)
+                    .with_quantity(stackable.quantity);
+                self.write_bytes(&item.into_bytes());
+            }
+            _ => {
+                self.write_u16(0);
+                self.write_u32(0);
+            }
+        }
+    }
+
+    fn write_item_full(&mut self, item: Option<&Item>) {
+        match item {
+            Some(Item::Equipment(equipment)) => {
+                self.write_equipment_item_full(Some(equipment));
+            }
+            Some(Item::Stackable(stackable)) => {
+                self.write_stackable_item_full(Some(stackable));
+            }
+            _ => {
+                self.write_u16(0);
+                self.write_u32(0);
+            }
+        }
+    }
+
+    fn write_item_full_money(&mut self, money: Money) {
+        let item = PacketStackableItemFull::new()
+            .with_item_type(31)
+            .with_quantity(money.0 as u32);
+        self.write_bytes(&item.into_bytes());
+    }
 }
