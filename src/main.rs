8adef52cf9a6d62e4a12a8ec847042183126b402
James Benton <james.benton2@gmail.com>
2022-01-11 13:27:32 +0000

    Add command line arguments to set bind ip and port.

--- src/main.rs
+++ src/main.rs
@@ -11,9 +11,11 @@ mod game;
 mod irose;
 mod protocol;
 
+use std::time::Instant;
+
+use clap::{App, Arg};
 use log::debug;
 use simplelog::*;
-use std::time::Instant;
 use tokio::net::TcpListener;
 
 use crate::protocol::server::{GameServer, LoginServer, WorldServer};
@@ -28,6 +30,41 @@ async fn main() {
     )
     .expect("Failed to initialise logging");
 
+    let app = App::new("rose-offline")
+        .arg(
+            Arg::new("ip")
+                .long("ip")
+                .help("Listen IP used for login, world, game servers")
+                .takes_value(true)
+                .default_value("127.0.0.1"),
+        )
+        .arg(
+            Arg::new("login-port")
+                .long("login-port")
+                .help("Port for login server")
+                .takes_value(true)
+                .default_value("29000"),
+        )
+        .arg(
+            Arg::new("world-port")
+                .long("world-port")
+                .help("Port for world server")
+                .takes_value(true)
+                .default_value("29100"),
+        )
+        .arg(
+            Arg::new("game-port")
+                .long("game-port")
+                .help("Port for login server")
+                .takes_value(true)
+                .default_value("29200"),
+        );
+    let matches = app.get_matches();
+    let listen_ip = matches.value_of("ip").unwrap();
+    let login_port = matches.value_of("login-port").unwrap();
+    let world_port = matches.value_of("world-port").unwrap();
+    let game_port = matches.value_of("game-port").unwrap();
+
     let started_load = Instant::now();
     let game_data = irose::get_game_data();
     debug!("Time take to read game data {:?}", started_load.elapsed());
@@ -38,7 +75,9 @@ async fn main() {
     });
 
     let mut login_server = LoginServer::new(
-        TcpListener::bind("127.0.0.1:29000").await.unwrap(),
+        TcpListener::bind(format!("{}:{}", listen_ip, login_port))
+            .await
+            .unwrap(),
         irose::login_protocol(),
         game_control_tx.clone(),
     )
@@ -47,7 +86,9 @@ async fn main() {
 
     let mut world_server = WorldServer::new(
         String::from("_WorldServer"),
-        TcpListener::bind("127.0.0.1:0").await.unwrap(),
+        TcpListener::bind(format!("{}:{}", listen_ip, world_port))
+            .await
+            .unwrap(),
         irose::world_protocol(),
         game_control_tx.clone(),
     )
@@ -57,7 +98,9 @@ async fn main() {
     let mut game_server = GameServer::new(
         String::from("GameServer"),
         world_server.get_entity(),
-        TcpListener::bind("127.0.0.1:0").await.unwrap(),
+        TcpListener::bind(format!("{}:{}", listen_ip, game_port))
+            .await
+            .unwrap(),
         irose::game_protocol(),
         game_control_tx.clone(),
     )
