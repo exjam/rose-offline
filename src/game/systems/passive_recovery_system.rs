99909ce9a60a5c996ed2c4ab3f38196407e32d93
James Benton <james.benton2@gmail.com>
2022-07-26 11:15:05 +0100

    Fix passive hp / mp recovery for dead entities.

--- src/game/systems/passive_recovery_system.rs
+++ src/game/systems/passive_recovery_system.rs
@@ -22,39 +22,37 @@ pub fn passive_recovery_system(
     game_data: Res<GameData>,
     time: Res<ServerTime>,
 ) {
-    query.for_each_mut(
-        |(
-            mut passive_recovery_time,
-            ability_values,
-            command,
-            mut health_points,
-            mut mana_points,
-        )| {
-            passive_recovery_time.time += time.delta;
+    for (mut passive_recovery_time, ability_values, command, mut health_points, mut mana_points) in
+        query.iter_mut()
+    {
+        passive_recovery_time.time += time.delta;
 
-            if passive_recovery_time.time > RECOVERY_INTERVAL {
-                passive_recovery_time.time -= RECOVERY_INTERVAL;
+        if passive_recovery_time.time > RECOVERY_INTERVAL {
+            passive_recovery_time.time -= RECOVERY_INTERVAL;
 
-                let recovery_state = if command.is_sit() {
-                    PassiveRecoveryState::Sitting
-                } else {
-                    PassiveRecoveryState::Normal
-                };
-
-                let recover_hp = game_data
-                    .ability_value_calculator
-                    .calculate_passive_recover_hp(ability_values, recovery_state);
-                let recover_mp = game_data
-                    .ability_value_calculator
-                    .calculate_passive_recover_mp(ability_values, recovery_state);
-
-                health_points.hp = i32::min(
-                    health_points.hp + recover_hp,
-                    ability_values.get_max_health(),
-                );
-                mana_points.mp =
-                    i32::min(mana_points.mp + recover_mp, ability_values.get_max_mana());
+            if command.is_dead() {
+                // No recovery for the dead
+                continue;
             }
-        },
-    );
+
+            let recovery_state = if command.is_sit() {
+                PassiveRecoveryState::Sitting
+            } else {
+                PassiveRecoveryState::Normal
+            };
+
+            let recover_hp = game_data
+                .ability_value_calculator
+                .calculate_passive_recover_hp(ability_values, recovery_state);
+            let recover_mp = game_data
+                .ability_value_calculator
+                .calculate_passive_recover_mp(ability_values, recovery_state);
+
+            health_points.hp = i32::min(
+                health_points.hp + recover_hp,
+                ability_values.get_max_health(),
+            );
+            mana_points.mp = i32::min(mana_points.mp + recover_mp, ability_values.get_max_mana());
+        }
+    }
 }
