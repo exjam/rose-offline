00e481e82a76dc9e7f694db4277941e29b88094f
James Benton <james.benton2@gmail.com>
2022-08-27 11:41:35 +0100

    Add SkillList.find_skill_level

--- rose-game-common/src/components/skill_list.rs
+++ rose-game-common/src/components/skill_list.rs
@@ -1,7 +1,7 @@
 use bevy::ecs::prelude::Component;
 use serde::{Deserialize, Serialize};
 
-use rose_data::{SkillData, SkillId, SkillPageType};
+use rose_data::{SkillData, SkillDatabase, SkillId, SkillPageType};
 
 #[derive(Copy, Clone, Debug, Deserialize, Serialize)]
 pub struct SkillSlot(pub SkillPageType, pub usize);
@@ -42,6 +42,26 @@ impl SkillPage {
             .enumerate()
             .find(|(_, slot)| **slot == Some(skill_data.id))
             .map(|(index, _)| (SkillSlot(self.page_type, index), skill_data.id))
+    }
+
+    pub fn find_skill_level(
+        &self,
+        skill_database: &SkillDatabase,
+        base_skill_id: SkillId,
+    ) -> Option<(SkillSlot, SkillId, u32)> {
+        for (index, slot) in self.skills.iter().enumerate() {
+            if let Some(skill_data) = slot.and_then(|id| skill_database.get_skill(id)) {
+                if skill_data.base_skill_id == Some(base_skill_id) {
+                    return Some((
+                        SkillSlot(self.page_type, index),
+                        skill_data.id,
+                        skill_data.level,
+                    ));
+                }
+            }
+        }
+
+        None
     }
 }
 
@@ -84,6 +104,19 @@ impl SkillList {
     pub fn find_skill_exact(&self, skill_data: &SkillData) -> Option<(SkillSlot, SkillId)> {
         self.get_page(skill_data.page)
             .and_then(|page| page.find_skill_exact(skill_data))
+    }
+
+    pub fn find_skill_level(
+        &self,
+        skill_database: &SkillDatabase,
+        base_skill_id: SkillId,
+    ) -> Option<(SkillSlot, SkillId, u32)> {
+        if let Some(skill_data) = skill_database.get_skill(base_skill_id) {
+            self.get_page(skill_data.page)
+                .and_then(|page| page.find_skill_level(skill_database, base_skill_id))
+        } else {
+            None
+        }
     }
 
     pub fn get_skill(&self, skill_slot: SkillSlot) -> Option<SkillId> {
